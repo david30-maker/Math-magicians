12af8fbd23197d30d178596ae3c81fa895d5d989
david30-maker <95672514+david30-maker@users.noreply.github.com>
2023-03-28 17:27:14 +0100

    Create calculator componet

---
+++ src/components/Calculator.js
@@ -0,0 +1,27 @@
+/*eslint-disable*/
+export const Wrapper = ({ children }) => <div className="wrapper">{children}</div>;
+
+export const Screen = ({ value }) => (
+    <div className="screen" mode="single" max={70}>
+        {value}
+    </div>
+);
+
+export const ButtonBox = ({ children }) => <div className="buttonBox">{children}</div>;
+
+const getStyleName = btn => {
+    const className = {
+        '0': 'zero',
+        '/': 'operator',
+        'x': 'operator',
+        '-': 'operator',
+        '+': 'operator',
+        '=': 'equals'
+    }
+    return className[btn]
+}
+export const Button = ({ value, onClick }) => (
+    <button className={`${getStyleName(value)} button`} onClick={onClick}>
+        {value}
+    </button>
+);
