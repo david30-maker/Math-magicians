2c962e919c43c781eedc57f6800520af545f6427
david30-maker <95672514+david30-maker@users.noreply.github.com>
2023-03-30 22:37:26 +0100

    fix linter errors

--- src/components/quotes/Quote.js
+++ src/components/quotes/Quote.js
@@ -1,60 +1,57 @@
 import React, { useState, useEffect } from 'react';
 
 const Quote = () => {
-    const [quote, setQuote] = useState('');
-    const [loading, setLoading] = useState(true);
-    const [error, setError] = useState(false);
-    const [mounted, setMounted] = useState(false);
+  const [quote, setQuote] = useState('');
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false);
+  const [mounted, setMounted] = useState(false);
 
-    useEffect(() => {
-        setMounted(true);
-        return () => {
-            setMounted(false);
-        };
-    }, []);
+  useEffect(() => {
+    setMounted(true);
+    return () => {
+      setMounted(false);
+    };
+  }, []);
 
-    useEffect(() => {
-        const fetchQuote = async () => {
-            setLoading(true);
-            setError(false);
+  useEffect(() => {
+    const fetchQuote = async () => {
+      setLoading(true);
+      setError(false);
 
-            try {
-                const res = await fetch('https://api.api-ninjas.com/v1/quotes?category=happiness', {
-                    headers: {
-                        'X-Api-Key': 'jzzfew774Kqg+nf2AkhCAw==pIwj1VNrBEBwQ0lW',
-                        'Content-Type': 'application/json',
-                    },
-                });
+      try {
+        const res = await fetch('https://api.api-ninjas.com/v1/quotes?category=happiness', {
+          headers: {
+            'X-Api-Key': 'jzzfew774Kqg+nf2AkhCAw==pIwj1VNrBEBwQ0lW',
+            'Content-Type': 'application/json',
+          },
+        });
 
-                const data = await res.json();
-                if (mounted) {
-                    // console.log(data); PLEASE DO NOT REMOVE THIS LINE ,
-                    // for some reason the api does not properly work without it
-                    // There is some issue with api ninja.
-                    setQuote(data[0].quote);
-                    setLoading(false);
-                }
-            } catch (error) {
-                setError(true);
-                setLoading(false);
-            }
-        };
-        fetchQuote();
-    }, [mounted]);
+        const getData = await res.json();
+        if (mounted) {
+          setQuote(getData[0].quote);
+          setLoading(false);
+        }
+      } catch (error) {
+        setError(true);
+        setLoading(false);
+      }
+    };
+    fetchQuote();
+  }, [mounted]);
 
-    if (loading) {
-        return <span>Loading...</span>;
-    }
+  if (loading) {
+    return <span>Loading..</span>;
+  }
 
-    if (error) {
-        return <span>Something went wrong...</span>;
-    }
+  if (error) {
+    return <span>Something went wrong...</span>;
+  }
 
-    return (
-        <div>
-            <span>{quote}</span>
-        </div>
-    );
+  return (
+    <div>
+      <span>{quote}</span>
+    </div>
+  );
 };
 
 export default Quote;
